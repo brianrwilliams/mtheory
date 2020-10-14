9cbb26654070837eabf2aa0f9b8dbedecbb7912f
Brian Williams <brianwilliams@Brians-MacBook-Pro.local>
2020-10-14 14:03:33 +0100

    update

---
+++ .auctex-auto/paper.el
@@ -0,0 +1,51 @@
+(TeX-add-style-hook
+ "paper"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:orgea6ebfa"
+    "sec:org23fe8d4"
+    "sec:orgd709323"
+    "sec:orgec1821a"
+    "sec:org2b54a17"
+    "sec:org553e6b1"
+    "sec:orgefb666b"
+    "sec:org22f4829"
+    "sec:org9f736c1"
+    "sec:org0b90e8b"
+    "sec:org3b2a900"
+    "sec:org02b0f82"
+    "sec:org59003d2"
+    "sec:org55d712d"
+    "sec:org7c4a220"
+    "sec:orgc520304"))
+ :latex)
+
